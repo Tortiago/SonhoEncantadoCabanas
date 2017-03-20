bd133390ec039d389633aee51ff4c1dc37647529
Tiago <Tiago@192.168.0.12>
2017-03-19 21:10:04 -0300

    Subindo projeto para github

---
+++ Cabana/WebContent/js/init.js
@@ -0,0 +1,33 @@
+(function($){
+	$(function(){
+
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+
+		$(document).ready(function(){
+			$('.carousel').carousel();
+		}); 
+		
+		$(document).ready(function(){
+			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+			$('.modal').modal();
+		});
+		$('#modal1').modal('open');
+		$('#modal1').modal('close');
+		$('.modal').modal({
+			dismissible: true, // Modal can be dismissed by clicking outside of the modal
+			opacity: .5, // Opacity of modal background
+			inDuration: 300, // Transition in duration
+			outDuration: 200, // Transition out duration
+			startingTop: '4%', // Starting top style attribute
+			endingTop: '10%', // Ending top style attribute
+			ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+				alert("Ready");
+				console.log(modal, trigger);
+			},
+			complete: function() { alert('Closed'); } // Callback for Modal close
+		}
+		);
+
+	}); // end of document ready
+})(jQuery); // end of jQuery name space

a0a23179878a907c5c4b4b306631294a9d1af910
Tiago <Tiago@192.168.0.12>
2017-03-25 19:51:12 -0300

    Finalizando modal de detalhes

--- Cabana/WebContent/js/init.js
+++ Cabana/WebContent/js/init.js
@@ -12,10 +12,29 @@
 			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 			$('.modal').modal();
 		});
-		$('#modal1').modal('open');
-		$('#modal1').modal('close');
-		$('.modal').modal({
-			dismissible: true, // Modal can be dismissed by clicking outside of the modal
+		
+		$('#modalDetalhesDeJogos').modal('open');
+		$('#modalDetalhesDeJogos').modal('close');
+		$('.modalDetalhesDeJogos').modal({
+			dismissible: false, // Modal can be dismissed by clicking outside of the modal
+			opacity: .5, // Opacity of modal background
+			inDuration: 300, // Transition in duration
+			outDuration: 200, // Transition out duration
+			startingTop: '4%', // Starting top style attribute
+			endingTop: '10%', // Ending top style attribute
+			ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+				alert("Ready");
+				console.log(modal, trigger);
+			},
+			complete: function() { alert('Closed'); } // Callback for Modal close
+		}
+		);
+		
+		
+		$('#modalInformacoes').modal('open');
+		$('#modalInformacoes').modal('close');
+		$('.modalInformacoes').modal({
+			dismissible: false, // Modal can be dismissed by clicking outside of the modal
 			opacity: .5, // Opacity of modal background
 			inDuration: 300, // Transition in duration
 			outDuration: 200, // Transition out duration
